161f51b0363d7bc14630f1cd8efe137c6d28d100
SamBerna <111804402+SamBerna@users.noreply.github.com>
2023-04-24 17:46:57 -0600

    Tried to make it run faster and added simple email
    It should only check for bodies every 10 loops. All the email info is hard coded so configure as needed. Should only send an email every 5 minutes

--- surveillance.py
+++ surveillance.py
@@ -3,6 +3,22 @@ import time
 import datetime
 from picamera2 import Picamera2
 
+# email stuff -------------------------------------------------
+import smtplib, ssl
+port = 465  #default port
+last_email_sent = time.time()
+email_send_rate = 300
+
+email_to = "sberna@colostate.edu"  #change this to your email
+email_from = "cs370groupemail@gmail.com"
+email_password = "cs370emailpassword"
+
+email_message = """\
+Subject: Security Alert
+
+The camera has registered someone at your door."""
+# -------------------------------------------------------------
+
 # Configure Camera
 piCam = Picamera2() # Create picamera object
 piCam.preview_configuration.main.size=(640,480) # Setting up camera configuration
@@ -12,7 +28,6 @@ piCam.preview_configuration.align()
 piCam.configure("preview")
 piCam.start() # Start camera
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml") # Add facial detection
 body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml") # Add full body detection
 
 detection = False
@@ -20,16 +35,24 @@ detection_stopped_time = None
 timer_started = False
 SECONDS_TO_RECORD_AFTER_DETECTION = 10
 
+# Check for bodies less often to boost performance ---
+RATE_TO_CHECK_DETECTION = 10
+counter = 0
+# ----------------------------------------------------
+
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 
 while True:
     frame = piCam.capture_array() # Grab frame
     
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # Convert image to grayscale
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
+    if (counter >= RATE_TO_CHECK_DETECTION):
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # Convert image to grayscale
+        bodies = face_cascade.detectMultiScale(gray, 1.3, 5)
+        counter = 0
+    else:
+        counter += 1
     
-    if len(faces) + len(bodies) > 0: # Face or body is in frame
+    if len(bodies) > 0: # Body is in frame
         if detection: # If detecting something, keep recording
             timer_started = False
         else: # IF not recording, start a new video
@@ -44,15 +67,23 @@ while True:
                 timer_started = False
                 out.release()
                 print("Stopped recording.")
-        else:
+                #Send email when video stops --------------------------------------------------
+                if ((time.time() - last_email_sent) > email_send_rate):
+                    context = ssl.create_default_context()
+                    with smtplib.SMTP_SSL("smpt.gmail.com", port, context=context) as server:
+                        server.login(email_from, email_password)
+                        server.sendmail(email_from, email_to, email_message)
+                    last_email_sent = time.time()
+                # ------------------------------------------------------------------------------
+            else:
             timer_started = True
             detection_stopped_time = time.time()
     
     if detection:    
         out.write(frame) # Write frame to video if face or body is in frame
     
-    for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3) # Draw rectangle around face in frame
+    #for (x, y, width, height) in faces:
+    #    cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3) # Draw rectangle around face in frame
     
     cv2.imshow("piCam", frame) # Display frame
     
